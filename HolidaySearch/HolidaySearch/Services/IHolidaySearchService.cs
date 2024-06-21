459c89ff48190a7237fe781b3904813124f7886d
runtime-error786 <mustafa782a@gmail.com>
2024-06-21 14:27:44 +0500

    Search service second file added

---
+++ HolidaySearch/HolidaySearch/Services/IHolidaySearchService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HolidaySearch.Models;
+
+
+
+namespace HolidaySearch.Services
+{
+    public interface IHolidaySearchService
+    {
+        IEnumerable<HolidaySearch.Models.Holiday> Search(string departingFrom, string travelingTo, DateTime departureDate, int duration);
+    }
+}
+

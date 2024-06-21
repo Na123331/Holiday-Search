70c4d88150af52ed1fd322750aa1386ed4193ca2
runtime-error786 <mustafa782a@gmail.com>
2024-06-21 14:26:26 +0500

    Hotel model added

---
+++ HolidaySearch/HolidaySearch/Models/Hotel.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HolidaySearch.Models
+{
+    using Newtonsoft.Json;
+    using System;
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [JsonProperty("arrival_date")]
+        public DateTime ArrivalDate { get; set; }
+        public decimal PricePerNight { get; set; }
+
+        [JsonProperty("local_airports")]
+        public List<string> LocalAirports { get; set; }
+        public int Nights { get; set; }
+    }
+}
